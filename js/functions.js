9d72bdec8dda0e065ae2f8702203fd8b571456ca
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-03-19 23:29:57 -0300

    Api

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,9 @@ window.onload = () => {
     initialize = () => {
         var mapProp = {
             center: new google.maps.LatLng(-27.609959, -48.576585),
-            scrollWhell: false,
+            scrollwhell: false,
             zoom: 12,
-            MapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.SATELLITE
         }
 
         map = new google.maps.Map(document.getElementById("map"), mapProp);
